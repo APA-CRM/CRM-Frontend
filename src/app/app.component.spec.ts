3f451023fa99926ffc7835d2ee7631202caecae1
Pavel Tseva <paveltseva@gmail.com>
2025-03-22 12:50:39 +0200

    Add tests for auth-service and refactor tests in app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { AuthService } from './core/services/auth-service.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        AuthService, 
+        provideHttpClient(),    
+        provideHttpClientTesting(),
+      ]
     }).compileComponents();
   });
 
@@ -13,19 +21,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'CRM-Frontend' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('CRM-Frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CRM-Frontend');
   });
 
 });
--- src/app/core/services/auth-service.service.spec.ts
+++ src/app/core/services/auth-service.service.spec.ts
@@ -1,16 +1,52 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth-service.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { EnvironmentDev } from '../environment/environment.dev';
 
 describe('AuthServiceService', () => {
   let service: AuthService;
+  let controller: HttpTestingController;
+  let env: EnvironmentDev;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(AuthService);
+    controller = TestBed.inject(HttpTestingController);
+    env = TestBed.inject(EnvironmentDev);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('sign in request should return observable',  () => {
+    
+    const expectedValue = {token: 'token', tokenType: 'tokenType', refreshToken: 'refreshToken'};
+    
+    
+    service.signUp({login: 'login', password: 'password'})
+    .subscribe({
+      next: details => {
+        expect(details).toBe(expectedValue)
+      }
+    });
+    
+    const request = controller.expectOne(env.apiUrl + '/api/auth/sign-up');
+    
+    request.flush(expectedValue);
+  })
+
+  it('save credentials should save them', () => {
+    const expectedValue = {token: 'token', tokenType: 'tokenType', refreshToken: 'refreshToken'};
+
+    service.saveCredential(expectedValue);
+
+    expect(localStorage.getItem('token')).toBe(expectedValue.token);
+    expect(localStorage.getItem('tokenType')).toBe(expectedValue.tokenType);
+    expect(localStorage.getItem('refreshToken')).toBe(expectedValue.refreshToken);
+  })
+
 });
