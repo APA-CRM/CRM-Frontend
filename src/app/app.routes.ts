117143b575caea255b60d8b95dcfc49f472411c3
Pavel Tseva <paveltseva@gmail.com>
2025-04-19 20:09:33 +0300

    Add home page and side bar components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { CreateOrganizationComponent } from './components/create-organization/create-organization.component';
 import { ChooseOrganizationComponent } from './components/choose-organization/choose-organization.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'registration', component: RegistrationComponent},
     {path: 'organization/create', component: CreateOrganizationComponent},
-    {path: 'organization/choose', component: ChooseOrganizationComponent}
+    {path: 'organization/choose', component: ChooseOrganizationComponent},
+    {path: '', component: HomeComponent, children: [
+            
+        ]
+    }
 ];
