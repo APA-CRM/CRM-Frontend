7174c29599c28d488738746e312b9d81cf431c52
Pavel Tseva <paveltseva@gmail.com>
2025-04-20 15:58:39 +0300

    Connect home-page with backend

---
+++ src/app/core/services/user.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { EnvironmentDev } from '../environment/environment.dev';
+import { Observable } from 'rxjs';
+import { UserModel } from '../../models/user-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private readonly BASE_URI = '/api/users'
+
+  constructor(
+    private http: HttpClient,
+    private env: EnvironmentDev
+  ) {}
+
+  public getAuthenticatedUser(): Observable<UserModel> {
+    return this.http.get<UserModel>(this.env.apiUrl + this.BASE_URI + '/me');
+  }
+}
---
+++ src/app/models/user-model.ts
@@ -0,0 +1,10 @@
+export interface UserModel {
+    id: number
+    login: string
+    email: string
+    fullName: string
+    firstName: string
+    lastName: string
+    createdAt: Date
+    updateAt: Date
+}
