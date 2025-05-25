211cd4c566f3daa239b2dce3bc5f6e4f8536ba9c
Pavel Tseva <paveltseva@gmail.com>
2025-05-25 14:43:58 +0300

    Rename dir of create role component

--- src/app/components/home-page/roles/roles.component.ts
+++ src/app/components/home-page/roles/roles.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { DividerModule } from 'primeng/divider';
-import { CreateRoleComponent } from "../create-update-role/create-role.component";
+import { CreateRoleComponent } from "../create-role/create-role.component";
 
 @Component({
   selector: 'app-roles',
   imports: [
     DividerModule,
     CreateRoleComponent
-],
+  ],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
