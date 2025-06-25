cf6d69e0400d6c96111bf134a381af90536b92f8
Pavel Tseva <paveltseva@gmail.com>
2025-06-25 21:14:08 +0300

    Add service and models for Invitation API

---
+++ src/app/core/services/organization-invitation.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {EnvironmentDev} from '../environment/environment.dev';
+import {InvitationModel} from '../../models/invitations/invitation-model';
+import {Observable} from 'rxjs';
+import {InvitationRequest} from '../../models/invitations/invitation-request';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrganizationInvitationService {
+
+  private readonly BASE_URI = '/api/organizations'
+
+  constructor(
+    private http: HttpClient,
+    private env: EnvironmentDev
+  ) { }
+
+  public getInvitation(invitationId: string): Observable<InvitationModel> {
+    return this.http.get<InvitationModel>(this.env.apiUrl + this.BASE_URI + `/invitations/${invitationId}`);
+  }
+
+  public createInvitation(organizationId: number, userId: number): Observable<InvitationModel> {
+    let body: InvitationRequest = {
+      userId: userId,
+    };
+
+    return this.http.post<InvitationModel>(this.env.apiUrl + this.BASE_URI + `/${organizationId}/invitations`, body)
+  }
+
+  public approveInvitation(invitationId: string): Observable<InvitationModel> {
+    return this.http.patch<InvitationModel>(this.env.apiUrl + this.BASE_URI + `/invitations/${invitationId}/approve`, {});
+  }
+
+  public declineInvitation(invitationId: string): Observable<InvitationModel> {
+    return this.http.patch<InvitationModel>(this.env.apiUrl + this.BASE_URI + `/invitations/${invitationId}/decline`, {});
+  }
+
+}
