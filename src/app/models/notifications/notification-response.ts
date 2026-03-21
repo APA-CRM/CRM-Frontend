1aa9dccaaf92f33e06fe6727b826d6c72e5a72d6
Pavel Tseva <paveltseva@gmail.com>
2026-03-21 14:19:56 +0200

    Provide better integration with the notifications
    - Provide custom implementation of toast when a notification is about adding user to organization or user has been invited to an organization

--- src/app/models/notifications/notification-response.ts
+++ src/app/models/notifications/notification-response.ts
@@ -1,6 +1,8 @@
+import {NotificationType} from '../../core/enums/notification-type';
+
 export interface NotificationResponse {
   title: string;
-  messageCode: string;
+  messageCode: NotificationType;
   message: string;
   details: Record<string, any>;
 }
